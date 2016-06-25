673be5099f5bce31a74aba5b9f13d0b27973dfcc
jmuldvp <jmul.dvp@gmail.com>
2016-06-25 07:58:56 +0000

    Assignment 19 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,17 @@ class AddressBook
         end
         entries.insert(index, Entry.new(name, phone_number, email))
     end
+    
+    def remove_entry(name, phone_number, email)
+        index = 0
+        del = nil
+        entries.each do |entry|
+            if name == entry.name && phone_number == entry.phone_number && email == entry.email
+                del = entry
+                break
+            end
+            index += 1
+        end
+        entries.delete(del)
+    end
 end
